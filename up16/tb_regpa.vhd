24b7f51202f93665365062255cd6ee488eaa4ec7
Tomás Vidal <tomi_vidal_99@hotmail.com>
2023-05-22 10:38:59 -0300

    Modificado, pero el puerto A y B no estan conectados

--- up16/tb_regpa.vhd
+++ up16/tb_regpa.vhd
@@ -8,15 +8,6 @@ end entity;
 
 architecture A1 of tb_regpa is
 
-  component up16
-    Port (
-      clock_up16    : in std_logic;
-      reset_up16    : in std_logic;
-      PORT_A        : out std_logic_vector (15 downto 0);
-      PORT_B        : in std_logic_vector (15 downto 0)
-    );
-  end component;
-
   component REGPA
 	  Port ( entradaregpa : in std_logic_vector(15 downto 0);
 			 selcoderegpa : in std_logic_vector(3 downto 0);
@@ -27,52 +18,42 @@ architecture A1 of tb_regpa is
 			 clockregpa   : in std_logic); 
   end component;
 
-  signal clk_50Mhz: std_logic := '0'; -- señal de reloj de 50 Mhz como la que tiene la placa
-  signal PORT_A: std_logic_vector(15 downto 0) := X"0000";
   signal resetregpa: std_logic := '0';
-  --signal clk100Hz: std_logic := '0';
-  --signal entradaregpa : std_logic_vector(15 downto 0) := "0000000010000000";
-	--signal selcoderegpa : std_logic_vector(3 downto 0) := "0000";
-	--signal salidaregpa  : std_logic_vector(15 downto 0) := X"0000";
-	--signal opselecregpa : std_logic_vector (2 downto 0) := "010";
-	--signal salidacode   : std_logic_vector (15 downto 0) := X"0000";
+  signal clk100Hz: std_logic := '0';
+  signal entradaregpa : std_logic_vector(15 downto 0) := "0000000010000000";
+	signal selcoderegpa : std_logic_vector(3 downto 0) := "0000";
+	signal salidaregpa  : std_logic_vector(15 downto 0) := X"0000";
+	signal opselecregpa : std_logic_vector (2 downto 0) := "010";
+	signal salidacode   : std_logic_vector (15 downto 0) := X"0000";
 
 begin
 
-  UUT: up16 port map (
-    clock_up16 => clk_50Mhz,
-    reset_up16 => resetregpa,
-    PORT_A => PORT_A,
-    PORT_B => X"0000"
+  UUT: REGPA port map (
+    entradaregpa => entradaregpa,
+    selcoderegpa => selcoderegpa,
+    salidaregpa => salidaregpa,
+    opselecregpa => opselecregpa,
+    salidacode => salidacode,
+    resetregpa => resetregpa,
+    clockregpa => clk100Hz
   );
 
- -- UUT: REGPA port map (
- --   entradaregpa => entradaregpa,
- --   selcoderegpa => selcoderegpa,
- --   salidaregpa => salidaregpa,
- --   opselecregpa => opselecregpa,
- --   salidacode => salidacode,
- --   resetregpa => resetregpa,
- --   clockregpa => clk100Hz
- -- );
-
   -- Señal de reloj
-  clk_50Mhz <= not clk_50Mhz after 10 ns;
-  --clk100Hz <= not clk100Hz after 5 ms;
+  clk100Hz <= not clk100Hz after 5 ms;
 
   -- Generacion de estimulos
   estimulos:
   process is
   begin
 
-    resetregpa <= '0';
-    wait for 100 ns; -- despues de 1 ciclo de reloj
     resetregpa <= '1';
+    wait for 20 ms; -- despues de 1 ciclo de reloj
+    resetregpa <= '0';
 
---    selcoderegpa <= "0111";
---    wait for 20 ms;
---
---    selcoderegpa <= "1111";
+    selcoderegpa <= "0111";
+    wait for 20 ms;
+
+    selcoderegpa <= "1111";
 
     wait;
  
